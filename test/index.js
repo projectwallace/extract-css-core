384f376b33f4820f6798c627124a6b0f6bb51962
Bart Veneman <bartveneman@users.noreply.github.com>
2020-02-16 20:59:27 +0100

    improve test file structure

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const test = require('ava')
 const createTestServer = require('create-test-server')
-const {readFileSync} = require('fs')
+const {readFileSync, existsSync} = require('fs')
 const {resolve} = require('path')
 
 const extractCss = require('..')
@@ -10,119 +10,61 @@ const fixture = readFileSync(resolve(__dirname, 'fixture.css'), 'utf8')
 
 test.before(async () => {
 	server = await createTestServer()
-
 	server.get('/fixture.css', (req, res) => {
 		res.send(fixture)
 	})
+	function staticFile(req, res) {
+		const fileContents = readFileSync(resolve(__dirname, req.path.slice(1)), 'utf8')
+		res.send(fileContents)
+	}
+
+	server.get('/css-in-js.html', staticFile)
+	server.get('/inline-style-html.html', staticFile)
+	server.get('/inline-style-js.html', staticFile)
+	server.get('/link-tag-html.html', staticFile)
+	server.get('/link-tag-js.html', staticFile)
+	server.get('/style-tag-html.html', staticFile)
+	server.get('/style-tag-js.html', staticFile)
 })
 
 test.after(async () => {
 	await server.close()
 })
 
-test('it fetches css from a page with CSS in a server generated <link> inside the <head>', async t => {
-	const url = '/server-link-head'
-	server.get(url, (req, res) => {
-		res.send(`
-			<!doctype html>
-			<html>
-				<head>
-					<link rel="stylesheet" href="fixture.css" />
-				</head>
-			</html>
-		`)
-	})
-
-	const actual = await extractCss(server.url + url)
+test('it finds css in a <link> tag - HTML', async t => {
+	const actual = await extractCss(server.url + '/link-tag-html.html')
 	const expected = fixture
-
 	t.is(actual, expected)
 })
 
-test('it fetches css from a page with CSS in server generated <style> inside the <head>', async t => {
-	const url = '/server-style-head'
-	server.get(url, (req, res) => {
-		res.send(`
-			<!doctype html>
-			<style>${fixture}</style>
-		`)
-	})
-
-	const actual = await extractCss(server.url + url)
-	const expected = 'body { color: teal; }'
-
-	t.is(actual, expected)
-})
-
-test('it finds JS generated <link /> CSS', async t => {
-	const path = '/js-generated-link'
-	const cssInJsExampleHtml = readFileSync(
-		resolve(__dirname, 'js-create-link-element.html'),
-		'utf8'
-	)
-
-	server.get(path, (req, res) => {
-		res.send(cssInJsExampleHtml)
-	})
-
-	const actual = await extractCss(server.url + path)
+test('it finds css in a <link> tag - JS', async t => {
+	const actual = await extractCss(server.url + '/link-tag-js.html')
 	const expected = fixture
-
 	t.is(actual, expected)
 })
 
-test('it finds JS generated <style /> CSS', async t => {
-	const url = '/js-generated-js-style-tag'
-	const cssInJsExampleHtml = readFileSync(
-		resolve(__dirname, 'js-create-style-element.html'),
-		'utf8'
-	)
-	server.get(url, (req, res) => {
-		res.send(cssInJsExampleHtml)
-	})
-
-	const actual = await extractCss(server.url + url, {waitUntil: 'load'})
-	const expected = 'body { color: teal; }'
-
+test('it finds css in a <style> tag - HTML', async t => {
+	const actual = await extractCss(server.url + '/style-tag-html.html')
+	const expected = '.fixture { color: red; }'
 	t.is(actual, expected)
 })
 
-test('it finds css-in-js, like Styled Components', async t => {
-	const url = '/css-in-js'
-	const cssInJsExampleHtml = readFileSync(
-		resolve(__dirname, 'css-in-js.html'),
-		'utf8'
-	)
-	server.get(url, (req, res) => {
-		res.send(cssInJsExampleHtml)
-	})
-
-	const actual = await extractCss(server.url + url, {waitUntil: 'load'})
-	const expected = 'body { color: red; }.bcMPWx { color: blue; }'
-
+test('it finds css in a <style> tag - JS', async t => {
+	const actual = await extractCss(server.url + '/style-tag-js.html')
+	const expected = '.fixture { color: red; }'
 	t.is(actual, expected)
 })
 
-test('it combines server generated <link> and <style> tags with client side created <link> and <style> tags', async t => {
-	const path = '/kitchen-sink'
-	const kitchenSinkExample = readFileSync(
-		resolve(__dirname, 'kitchen-sink.html'),
-		'utf8'
-	)
-	server.get(path, (req, res) => {
-		res.send(kitchenSinkExample)
-	})
-
-	const actual = await extractCss(server.url + path)
-
-	t.true(actual.includes('content: "js-style";'))
-	t.true(actual.includes('content: "server-style";'))
-	t.true(actual.includes('body {'))
-	t.true(actual.includes('color: teal;'))
-	t.snapshot(actual)
+test('it finds css-in-js', async t => {
+	const actual = await extractCss(server.url + '/css-in-js.html')
+	const expected = '.bcMPWx { color: blue; }'
+	t.is(actual, expected)
 })
 
 test('it rejects if the url has an HTTP error status', async t => {
+	server.get('/404-page', (req, res) => {
+		res.status(404).send()
+	})
 	const urlWith404 = server.url + '/404-page'
 	await t.throwsAsync(extractCss(urlWith404), {
 		message: `There was an error retrieving CSS from ${urlWith404}.\n\tHTTP status code: 404 (Not Found)`
